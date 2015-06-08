5cfe35d4b3791b332d3f088559882f2514512436
Jason-LewisG <j.lewisg@live.com>
2015-06-08 00:07:37 -0400

    configuring for multiple APIs

--- app/src/main/java/comjason_lewisg/httpsgithub/boozic/MainActivity.java
+++ app/src/main/java/comjason_lewisg/httpsgithub/boozic/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,9 +36,15 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+
+            Toast.makeText(MainActivity.this, "You have pressed refresh", Toast.LENGTH_SHORT).show();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
