3c64fe85a4123934633f5f2ef723e205f0d405f3
Timmy Chandy <timmychandy@hotmail.com>
2015-10-16 23:44:20 -0400

    Store the Registration token to database

--- app/src/main/java/comjason_lewisg/httpsgithub/boozic/MainActivity.java
+++ app/src/main/java/comjason_lewisg/httpsgithub/boozic/MainActivity.java
@@ -9,6 +9,7 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.transition.Fade;
 import android.transition.Slide;
@@ -36,7 +37,11 @@ import com.google.android.gms.iid.InstanceID;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import comjason_lewisg.httpsgithub.boozic.Fragments.ThemeFragment;
 import comjason_lewisg.httpsgithub.boozic.Fragments.TopTensFragment;
@@ -616,21 +621,16 @@ public class MainActivity extends AppCompatActivity implements ThemeFragment.OnD
 
     public void getRegId() {
         GoogleCloudMessaging gcmObj;
-
-
         // Check if Google Play Service is installed in Device
         // Play services is needed to handle GCM stuffs
         if (checkPlayServices()) {
-
             // Register Device in GCM Server
             getTokenInBackground();
         }
-
-
     }
 
     /**
-     * Get a Instance ID authorization Token
+     * Get an authorization Token for GCM
      */
     public void getTokenInBackground() {
         new AsyncTask<Void, Void, String>() {
@@ -638,24 +638,68 @@ public class MainActivity extends AppCompatActivity implements ThemeFragment.OnD
             protected String doInBackground(Void... params) {
                 String token = "";
                 try {
-                    //TODO: change token
+                    //TODO: get Project number from global variable
                     InstanceID instanceID = InstanceID.getInstance(getApplicationContext());
-                     token = instanceID.getToken("845607826709",
-                            GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-
+                     token = instanceID.getToken("845607826709",GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
                 } catch (final IOException e) {
                     token = "Error :" + e.getMessage();
-
                 }
                 return token;
             }
 
             @Override
             protected void onPostExecute(String msg) {
-                //TODO: Remove this and store this to database
-                //Toast.makeText(applicationContext,"Registered with GCM Server successfully.nn"+ msg, Toast.LENGTH_SHORT).show();
-
+                // Add token to App Server
+                addToken(msg);
+               // Toast.makeText(applicationContext,"Registered with GCM Server successfully.nn"+ msg, Toast.LENGTH_SHORT).show();
             }
+        }.execute();
+    }
+
+    //This will add the token to App server in the background
+    public void addToken(final String token) {
+        new AsyncTask<Void, Void, String>() {
+
+        private Exception exception;
+        protected String doInBackground(Void... urls) {
+            try {
+                StringBuilder urlString = new StringBuilder();
+                String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(),Settings.Secure.ANDROID_ID);
+                //TODO: Store the Server IP in global locaiton
+                urlString.append("http://54.210.175.98:9080//api/GCM/addGCMRegkey?");
+                urlString.append("RegKey=").append(token);
+                urlString.append("&DeviceId=").append( android_id );
+
+                URL url = new URL(urlString.toString());
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            else
+            {
+                //Toast.makeText(applicationContext,"Regn Token Updated to database", Toast.LENGTH_SHORT).show();
+            }
+        }
+
         }.execute();
     }
 }
